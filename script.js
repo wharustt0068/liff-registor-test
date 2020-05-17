ef06432bf86e21d14be5e797791896553f36f174
wharustt0068 <53973630+wharustt0068@users.noreply.github.com>
2020-05-17 15:01:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
   
       async function getFriendship() {
         const friend = await liff.getFriendship()
-        document.getElementById("friendship").append(friend.friendFlag)
+       // document.getElementById("friendship").append(friend.friendFlag)
         if(!friend.friendFlag){
           if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
             window.location = "https://line.me/R/ti/p/@754yztxz"
@@ -16,8 +16,8 @@
   
       async function getUserProfile() {
         const profile = await liff.getProfile()
-        document.getElementById("userId").append(profile.userId)
-        document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+       // document.getElementById("userId").append(profile.userId)
+       // document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
         console.log(profile.userId);
         if(profile.userId == "U1bc5e8b9661c28a9b92eccacc2f93817"){
             window.location = "https://liff.line.me/1654201326-VyywNaoe"
@@ -30,7 +30,7 @@
      
       async function main() {
         await liff.init({ liffId: "1654201326-312kXbD4" })
-        document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+       // document.getElementById("isLoggedIn").append(liff.isLoggedIn())
         if (liff.isLoggedIn()){
           getUserProfile()
           getFriendship()
