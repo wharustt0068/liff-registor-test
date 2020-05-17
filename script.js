419d97ebcfbdfdf3cf8b921902796496fe153ea2
wharustt0068 <53973630+wharustt0068@users.noreply.github.com>
2020-05-17 14:03:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+    function logOut() {
+        liff.logout()
+        window.location.reload()
+      }
+  
+      function check() {
+        liff.getProfile().then(profile => {
+            const userProfile = profile.userId;
+            const email = liff.getDecodedIDToken().email;
+          }).catch(
+            err => console.error(err)
+          );
+       
+      }
+      function show(){
+        console.log(userProfile);
+      }
+  
+      async function getFriendship() {
+        const friend = await liff.getFriendship()
+        document.getElementById("friendship").append(friend.friendFlag)
+        if(!friend.friendFlag){
+          if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
+            window.location = "https://line.me/R/ti/p/@754yztxz"
+          }
+        }
+      }
+  
+      async function getUserProfile() {
+        const profile = await liff.getProfile()
+        document.getElementById("userId").append(profile.userId)
+        document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+      }
+  
+     
+      async function main() {
+        await liff.init({ liffId: "1654201326-312kXbD4" })
+        document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+        if (liff.isLoggedIn()){
+          getUserProfile()
+          getFriendship()
+        } else{
+          liff.login()
+        }
+        // createUniversalLink()
+      }
+      main()
