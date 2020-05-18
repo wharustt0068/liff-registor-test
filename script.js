e01c6f72964bfedf6c189aa8694ac702f3b78957
wharustt0068 <53973630+wharustt0068@users.noreply.github.com>
2020-05-18 15:45:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
+function logOut() {
+  liff.logout()
+  window.location.reload()
+}
 
-   function logOut() {
-        liff.logout()
-        window.location.reload()
-      }
-  
-      async function getFriendship() {
-        const friend = await liff.getFriendship()
-        if(!friend.friendFlag){
-          if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
-            window.location = "https://line.me/R/ti/p/@754yztxz"
-          }
-        }
-      }
-  
-      async function getUserProfile() {
-        const profile = await liff.getProfile()
-        let myObj = {
-          "user": [
-            { "userFId":"U1bc5e8b9661c28a9b92eccacc2f93817"},
-            { "name":"wasupon"},
-          ]
-         }
+async function getFriendship() {
+  const friend = await liff.getFriendship()
+  if(!friend.friendFlag){
+    if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
+      window.location = "https://line.me/R/ti/p/@754yztxz"
+    }
+  }
+}
 
-        console.log(profile.userId);
-        if(profile.userId == myObj.user[0].userFId){
-            window.location = "https://liff.line.me/1654201326-VyywNaoe"
-        }
-        else{
-            window.location = "https://liff.line.me/1654201326-jJvpYEX2"
-        }
+async function getUserProfile() {
+  const profile = await liff.getProfile()
+  let myObj = {
+      "userId_line": "U1bc5e8b9661c28a9b92eccacc2f93817",
+      "bank": "11223344",
+      "total": "1000"     
       }
-  
-     
-      async function main() {
-        await liff.init({ liffId: "1654201326-312kXbD4" })
-       // document.getElementById("isLoggedIn").append(liff.isLoggedIn())
-        if (liff.isLoggedIn()){
-          getUserProfile()
-          getFriendship()
-        } else{
-          liff.login()
-       }
-        // createUniversalLink()
-      }
-      main()
+
+  console.log(profile.userId);
+  if(profile.userId == myObj.userId_line){
+      window.location = "https://liff.line.me/1654201326-VyywNaoe"
+  }
+  else{
+      window.location = "https://liff.line.me/1654201326-jJvpYEX2"
+  }
+}
+
+
+async function main() {
+  await liff.init({ liffId: "1654201326-312kXbD4" })
+ // document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+  if (liff.isLoggedIn()){
+    getUserProfile()
+    getFriendship()
+  } else{
+    liff.login()
+ }
+  // createUniversalLink()
+}
+main()
