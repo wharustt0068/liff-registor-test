1e5e940bcc3c7f6164a48061b8ba4f84a05895df
wharustt0068 <53973630+wharustt0068@users.noreply.github.com>
2020-05-19 11:39:39 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-function logOut() {
-  liff.logout()
-  window.location.reload()
-}
 
-async function getFriendship() {
-  const friend = await liff.getFriendship()
-  if(!friend.friendFlag){
-    if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
-      window.location = "https://line.me/R/ti/p/@754yztxz"
-    }
-  }
-}
-
-  async function getUserProfile() {
-  const profile = await liff.getProfile()
-  console.log(profile.userId);
-  $.getJSON('https://db-tester-waawwaw.000webhostapp.com/', function(data) {
-    console.log(data.status)
-    let rddr = data.status
-  });
-  if(rddr == "member"){
-      window.location = "https://liff.line.me/1654201326-VyywNaoe"
-  }
-  else{
-      window.location = "https://liff.line.me/1654201326-jJvpYEX2"
-  }
-}
-
-
-async function main() {
-  await liff.init({ liffId: "1654201326-312kXbD4" })
- // document.getElementById("isLoggedIn").append(liff.isLoggedIn())
-  if (liff.isLoggedIn()){
-    getUserProfile()
-    getFriendship()
-  } else{
-    liff.login()
- }
-  // createUniversalLink()
-}
-main()
+    function logOut() {
+        liff.logout()
+        window.location.reload()
+      }
+  
+      async function getFriendship() {
+        const friend = await liff.getFriendship()
+       // document.getElementById("friendship").append(friend.friendFlag)
+        if(!friend.friendFlag){
+          if(confirm("คุณยังไม่ได้เพิ่ม Message API รบกวนเพิ่ม Message API")){
+            window.location = "https://line.me/R/ti/p/@754yztxz"
+          }
+        }
+      }
+  
+      async function getUserProfile() {
+        const profile = await liff.getProfile()
+       // document.getElementById("userId").append(profile.userId)
+       // document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+        console.log(profile.userId);
+        if(profile.userId == "U1bc5e8b9661c28a9b92eccacc2f93817"){
+            window.location = "https://liff.line.me/1654201326-VyywNaoe"
+        }
+        else{
+            window.location = "https://liff.line.me/1654201326-jJvpYEX2"
+        }
+      }
+  
+     
+      async function main() {
+        await liff.init({ liffId: "1654201326-312kXbD4" })
+       // document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+        if (liff.isLoggedIn()){
+          getUserProfile()
+          getFriendship()
+        } else{
+          liff.login()
+        }
+        // createUniversalLink()
+      }
+      main()
